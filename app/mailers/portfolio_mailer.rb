637c52fdf7deee9a3d4c9dc59711a9b859aa65e4
Ashlinn Brennan <asbren13@gmail.com>
2015-04-14 20:07:32 -0400

    updated portfolio mailers

--- app/mailers/portfolio_mailer.rb
+++ app/mailers/portfolio_mailer.rb
@@ -3,6 +3,7 @@ class PortfolioMailer < ActionMailer::Base
   attr_accessor :sender, :body
 
   def email(data)
+    @name = data['name']
     @sender = data['sender']
     @body = data['body']
     mail(subject: 'Correspondence from Portfolio Site',
